e07b653876e37dbc8925bfe505c59b166cbec30d
cyrilshanway <cyrilshanway@gmail.com>
2014-11-04 14:40:46 +0800

    Initial Commit

---
+++ ACGesture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ACGesture
+//
+//  Created by Cyrilshanway on 2014/11/4.
+//  Copyright (c) 2014年 Cyrilshanway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

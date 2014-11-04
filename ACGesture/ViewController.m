fa1b698dc08e15f291cfc62e9029090a365984ba
cyrilshanway <cyrilshanway@gmail.com>
2014-11-04 15:12:23 +0800

    add one & two tap gesture

--- ACGesture/ViewController.m
+++ ACGesture/ViewController.m
@@ -23,5 +23,30 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark --one tap-->背景顏色更改
+- (IBAction)oneTapRecognizer:(id)sender {
+    UITapGestureRecognizer *tapGesture = (UITapGestureRecognizer *)sender;
+    tapGesture.view.backgroundColor = [UIColor greenColor];
+    tapGesture.view.alpha = 1;
+}
+
+#pragma mark --two tap -->放大/縮回
+- (IBAction)twoTapRecognizer:(id)sender {
+    UITapGestureRecognizer *tapGesture = (UITapGestureRecognizer *)sender;
+    tapGesture.view.backgroundColor = [UIColor brownColor];
+    tapGesture.view.alpha = 1;
+    
+    
+    BOOL isScaled = ! CGAffineTransformIsIdentity(tapGesture.view.transform);
+    
+    if (!isScaled) {
+        CGAffineTransform scalingTransform;
+        scalingTransform = CGAffineTransformMakeScale(1.2, 1.2);
+        tapGesture.view.transform = scalingTransform;
+    } else {
+        tapGesture.view.transform = CGAffineTransformIdentity;
+    }
+}
+
 
 @end
